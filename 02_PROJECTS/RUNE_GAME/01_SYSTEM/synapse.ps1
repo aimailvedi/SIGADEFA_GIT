c83436e9f655126ff5bb9e6afd2f95769d5912ed
GenArt <aimailvedi@gmail.com>
2025-12-18 20:40:11 +0200

    Sync: Автоматическое сохранение состояния (2025-12-18 20:40:11)

---
+++ 02_PROJECTS/RUNE_GAME/01_SYSTEM/synapse.ps1
@@ -0,0 +1,11 @@
+﻿# FUNCTION: GIFT_EXCHANGE
+# Описывает передачу данных из одного фрактального узла в другой
+Function Send-Gift {
+    Param([string]$From, [string]$To, [hashtable]$Data)
+    
+    $logMsg = "[$(Get-Date -Format 'HH:mm:ss')] GIFT: Обмен инициирован. $From -> $To. Содержимое: $($Data.Keys)"
+    Write-Host $logMsg -ForegroundColor Magenta
+    
+    # Эмуляция передачи в Audit-Log проекта
+    $logMsg | Out-File -FilePath "D:\GENESIS_TREE\04_KNOWLEDGE\HUMUS\audit.log" -Append -Encoding utf8
+}
